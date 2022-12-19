e2c0b4de57e9b6729426fee73564e7f0e0ddef94
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-12-19 13:21:47 +0300

    Added first task

--- Homework_1/Task_1.py
+++ Homework_1/Task_1.py
@@ -9,3 +9,14 @@
 # 6 -> да
 # 7 -> да
 # 1 -> нет
+
+def day_of_week(day):
+    while not day.isdigit() or not 0 < int(day) < 8:
+        day = input("Entered not a digit or not from 1 to 7. Try again: ")
+    if 0 < int(day) < 6:
+        print("No. Today is a weekday")
+    else:
+        print("Yes! Today is a weekend")
+
+
+day_of_week(input("Input the number of the week: "))
