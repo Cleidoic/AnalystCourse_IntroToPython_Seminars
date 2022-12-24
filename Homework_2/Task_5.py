71189657ecf97ccb89cbec5324eae6a1cf344b83
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-12-24 08:51:51 +0300

    Started fifth task

--- Homework_2/Task_5.py
+++ Homework_2/Task_5.py
@@ -6,3 +6,25 @@
 # 10
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
+
+def get_num(message):
+    while True:
+        try:
+            int(message)
+            return int(message)
+        except ValueError:
+            message = input("Entered not a number. Try again: ")
+            continue
+
+
+def get_list(n):
+    res = []
+    for i in range(n):
+        res.append(i)
+    return res
+
+
+# def shuffle():
+
+
+print(get_list(get_num(input())))
