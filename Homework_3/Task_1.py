8ec4bbe02d76dd427b9f65e51dfb3e08a056c6c9
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-12-29 10:57:44 +0300

    Added first task

--- Homework_3/Task_1.py
+++ Homework_3/Task_1.py
@@ -15,3 +15,31 @@
 # [4, 2, 4, 9]
 # 8
 
+import random
+
+def get_num(message):
+    while True:
+        try:
+            int(message)
+            return int(message)
+        except ValueError:
+            message = input("Entered not a number. Try again: ")
+            continue
+
+
+def get_list(n):
+    res = []
+    for i in range(n):
+        res.append(round(random.random()*100))
+    print(res)
+    return res
+
+
+def sum_odd(list):
+    sum = 0
+    for i in range(0, len(list), 2):
+        sum += list[i]
+    return sum
+
+
+print(sum_odd(get_list(get_num(input("Enter the number: ")))))
