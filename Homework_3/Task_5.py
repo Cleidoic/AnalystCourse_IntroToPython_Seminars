306d2dbde6e449608d833efbf63a232a7aa9c0ad
Kirill <cleidoic@bk.ru>
2023-01-05 21:09:52 +0300

    Added fifth task

--- Homework_3/Task_5.py
+++ Homework_3/Task_5.py
@@ -12,3 +12,22 @@
 # out
 # 5 -3 2 -1 1 0 1 1 2 3 5
 
+def get_num(message=input("Enter the number: ")):
+    while True:
+        try:
+            int(message)
+            return int(message)
+        except ValueError:
+            message = input("Entered not a number. Try again: ")
+            continue
+
+def negafib(num):
+    a, b = 1, 1
+    list = [0]
+    for i in range(num):
+        list.append(a)
+        list.insert(0, a * (-1) ** i)
+        a, b = b, b + a
+    return list
+
+print(negafib(get_num()))
