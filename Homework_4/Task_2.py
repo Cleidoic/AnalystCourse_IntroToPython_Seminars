3571b64dfddc695f6b2ff4cbf4c83282c21db121
Kirill <cleidoic@bk.ru>
2023-01-18 17:19:51 +0300

    Added second task

--- Homework_4/Task_2.py
+++ Homework_4/Task_2.py
@@ -16,5 +16,16 @@
 # out
 # [2, 3, 3, 3, 5, 37]
 
+def find_prime_nums(num):
+    pr_fact = []
+    di = 2
+    while num > 1:
+        if num % di == 0:
+            pr_fact.append(di)
+            num /= di
+        else:
+            di += 1
+    return pr_fact
 
+print(find_prime_nums(int(input())))
 
