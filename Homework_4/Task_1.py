5051b9006f2c6b7d2ad6488c3cc16ba590462ef8
Kirill <cleidoic@bk.ru>
2023-01-18 16:26:13 +0300

    Added first task

--- Homework_4/Task_1.py
+++ Homework_4/Task_1.py
@@ -16,4 +16,11 @@
 # out
 # 8.988
 
+from decimal import Decimal
 
+def accuracy (num, acc):
+    number = Decimal(f"{num}")
+    return number.quantize(Decimal(f"{acc}"))
+
+print(accuracy(float(input("Enter a real number: ")),
+               float(input("Enter the required accuracy 0.001: "))))
